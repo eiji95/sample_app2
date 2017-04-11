358a8035ef8a5ededf8ca27b793a731f30cc3385
Eiji Isobe <azisove@gmail.com>
2017-04-11 03:16:20 +0000

    モーダル設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp2::Application.routes.draw do
   get "static/home"
   get "static/help"
+  root to:"static#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
